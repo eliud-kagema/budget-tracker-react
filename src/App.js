029522a148620ff33b55a81f901cc39fa6f7ee1d
Kagema <eliud.k.maina@gmail.com>
2024-11-27 23:40:26 +0300

    adding more tailwind to app.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => {
         transaction.id === updatedTransaction.id ? updatedTransaction : transaction
       )
     );
-    setTransactionToEdit(null); // Reset the edit form after updating
+    setTransactionToEdit(null);
   };
 
   const deleteTransaction = (id) => {
@@ -26,16 +26,14 @@ const App = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 flex justify-center items-center p-5">
-      <div className="max-w-5xl md:w-11/12 w-full bg-white shadow-xl rounded-lg p-8 space-y-8">
-        <h1 className="text-4xl font-extrabold text-center text-blue-700">Budget Tracker</h1>
-        
+    <div className="min-h-screen bg-gray-50 flex justify-center items-center p-6">
+      <div className="max-w-4xl w-full bg-white shadow-lg rounded-lg p-8 space-y-8">
+        <h1 className="text-4xl font-bold text-center text-blue-700">Budget Tracker</h1>
         <BudgetForm
           addTransaction={addTransaction}
           editTransaction={editTransaction}
           transactionToEdit={transactionToEdit}
         />
-        
         <TransactionList
           transactions={transactions}
           onDelete={deleteTransaction}
