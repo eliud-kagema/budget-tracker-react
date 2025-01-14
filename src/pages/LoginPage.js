376d741c42d89114af6feaddcfa969035f2a007f
Kagema <eliud.k.maina@gmail.com>
2025-01-14 20:54:50 +0300

    adding login page

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../redux/actions/authActions";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const error = useSelector((state) => state.auth.error);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(login(email, password));
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+    </div>
+  );
+};
+
+export default LoginPage;
